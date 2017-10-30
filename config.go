17e6c766560d2945b6ae9044baae2b113478175d
Chris Heilman <heilmanc@amazon.com>
2017-10-30 16:15:12 -0400

    Have it look at a couple of environment variables to find repos.

--- config.go
+++ config.go
@@ -39,42 +39,49 @@ var defaultGitRepoSearch = map[string]int{
 	os.ExpandEnv("$HOME"): 3,
 }
 
-func GetGitRepoSearchPaths() map[string]int {
-	repos := os.ExpandEnv("$SYSDASH_REPO_SEARCH_PATHS")
+var gitRepoSearchEnvironmentVariables = []string{"SYSDASH_REPO_SEARCH_PATHS", "GIT_REPO_SEARCH_PATH"}
 
-	if len(repos) <= 0 {
-		return defaultGitRepoSearch
-	} else {
-		retval := make(map[string]int, 0)
+func parseGitRepoSearchPaths(path string) map[string]int {
+	retval := make(map[string]int, 0)
 
-		// Parse it out.  Current format is path:depth,path:depth,path:depth...
-		pathDepths := strings.Split(repos, ",")
+	// Parse it out.  Current format is path:depth,path:depth,path:depth...
+	pathDepths := strings.Split(path, ",")
 
-		for _, pathDepth := range pathDepths {
-			parts := strings.Split(pathDepth, ":")
+	for _, pathDepth := range pathDepths {
+		parts := strings.Split(pathDepth, ":")
 
-			if len(parts) != 2 {
-				log.Printf("Error parsing pathDepth '%v'.  Part length: %d", pathDepth, len(parts))
+		if len(parts) != 2 {
+			log.Printf("Error parsing pathDepth '%v'.  Part length: %d", pathDepth, len(parts))
+		} else {
+			path := parts[0]
+			depth, depthErr := strconv.Atoi(parts[1])
+
+			if depthErr != nil {
+				log.Printf("Error converting depth part '%v': %v", parts[1], depthErr)
 			} else {
-				path := parts[0]
-				depth, depthErr := strconv.Atoi(parts[1])
-
-				if depthErr != nil {
-					log.Printf("Error converting depth part '%v': %v", parts[1], depthErr)
-				} else {
-					path = normalizePath(path)
-					retval[path] = depth
-				}
+				path = normalizePath(path)
+				retval[path] = depth
 			}
 		}
+	}
 
-		if len(retval) <= 0 {
-			log.Printf("Got no entries when parsing repos environment var: '%v'.  Using defaults.", repos)
-			return defaultGitRepoSearch
-		} else {
-			return retval
+	return retval
+}
+
+func GetGitRepoSearchPaths() map[string]int {
+	for _, path := range gitRepoSearchEnvironmentVariables {
+		myRepos := os.ExpandEnv("$" + path)
+
+		if len(myRepos) > 0 {
+			repos := parseGitRepoSearchPaths(myRepos)
+
+			if len(repos) > 0 {
+				return repos
+			}
 		}
 	}
+
+	return defaultGitRepoSearch
 }
 
 ////////////////////////////////////////////
